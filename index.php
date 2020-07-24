fd069af76f3bc045f00cac3571e1ab266dedec27
dharmrajyadav <dharmraj.yadav5@gmail.com>
2020-07-24 22:48:33 +0530

    interface commit

--- index.php
+++ index.php
@@ -1,7 +1,46 @@
 <?php
 
 
-echo "Hello !!";
+        interface square
+        {
+            public function calculate();
+        }
 
+
+        
+        interface cube
+        {
+            public function calculate();
+        }
+        
+        
+        
+        class Digit implements square
+        {
+            
+            
+            public function calculate()
+            {
+                echo "square";
+                // $square = $s * $s;
+                // return $square;
+            }
+            
+            
+            public static function x()
+            {
+                echo "Cube";
+                // $cube = $s * $s * $s;
+                // return $cube;
+            }
+            
+            
+        }
+        
+        
+        $obj = new Digit;
+        $obj.calculate();
+        
+    
 
 ?>
