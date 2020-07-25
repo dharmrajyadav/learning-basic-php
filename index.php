bcf996283700a299286d6327413bf714542559b2
dharmrajyadav <dharmraj.yadav5@gmail.com>
2020-07-25 13:15:22 +0530

    php basic

--- index.php
+++ index.php
@@ -1,46 +1,7 @@
 <?php
 
-		//interface 
-        interface square
-        {
-            public function calculate();
-        }
 
+		echo "Php Basic !!";
 
-        
-        interface cube
-        {
-            public function calculate();
-        }
-        
-        
-        
-        class Digit implements square
-        {
-            
-            
-            public function calculate()
-            {
-                echo "square";
-                // $square = $s * $s;
-                // return $square;
-            }
-            
-            
-            public static function x()
-            {
-                echo "Cube";
-                // $cube = $s * $s * $s;
-                // return $cube;
-            }
-            
-            
-        }
-        
-        
-        $obj = new Digit;
-        $obj.calculate();
-        
-    
 
 ?>
